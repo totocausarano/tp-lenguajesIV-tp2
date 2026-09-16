8591b42c357fc61cd7104ca1af45ea3f5bc65c08
Tobias Causarano <totocausarano@gmail.com>
2026-09-16 18:00:41 -0300

    Entrega Tp 2

--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -3,7 +3,7 @@ import PieDePagina from '../componentes/PieDePagina';
 export default function Inicio() {
   return (
     <div>
-      <h1>Página de Inicio</h1>
+      <h1>Página de Inicio </h1>
       <PieDePagina />
     </div>
   );
